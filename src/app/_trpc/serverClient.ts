cc3bfdc014a7f49cfdced6772fd81ad6e8c728f1
dangchinh25 <dangchinh2501@gmail.com>
2023-09-23 17:34:57 -0500

    Fix linting

--- src/app/_trpc/serverClient.ts
+++ src/app/_trpc/serverClient.ts
@@ -1,4 +1,6 @@
 import { appRouter } from '@/server';
 import { httpBatchLink } from '@trpc/client';
 
-export const serverClient = appRouter.createCaller( { links: [ httpBatchLink( { url: 'http://localhost:3000/api' } ) ] } );
+export const serverClient = appRouter.createCaller(
+    { links: [ httpBatchLink( { url: 'http://localhost:3000/api' } ) ] }
+);
