008cba1999450f93b1362e760acd9a85a261db30
dangchinh25 <dangchinh2501@gmail.com>
2023-09-23 19:25:00 -0500

    Update drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,9 @@
 import { type Config } from 'drizzle-kit';
 import { env } from './config';
 
-const DATABASE_URL = `postgresql://${ env.POSTGRES_USER }:${ env.POSTGRES_PASSWORD }@${ env.POSTGRES_HOST }:${ env.POSTGRES_PORT }/${ env.POSTGRES_DB }?sslmode=verify-full`;
-
 export default {
     schema: './src/db/schema.ts',
     out: './drizzle',
     driver: 'pg',
-
-    // TODO template literal may not work here
-    dbCredentials: { connectionString: DATABASE_URL }
+    dbCredentials: { connectionString: env.DATABASE_URL }
 } satisfies Config;
