8740a1b4ef48bb642997051bf5b5f7f5dc88fb14
dangchinh25 <dangchinh2501@gmail.com>
2023-09-24 11:50:02 -0500

    Add delivered at column

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -8,6 +8,7 @@ export const Webhook = pgTable( 'webhooks', {
     payload: jsonb( 'payload' ).notNull(),
     deliveryAddress: text( 'delivery_address' ).notNull(),
     attemptNumber: integer( 'attempt_number' ).notNull(),
+    deliveredAt: timestamp( 'delivered_at', { withTimezone: true } ),
     createdAt: timestamp( 'created_at', { withTimezone: true } ).defaultNow(),
     updated: timestamp( 'updated_at', { withTimezone: true } )
 } );
