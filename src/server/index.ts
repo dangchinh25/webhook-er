d689ec8fc5253e06a462a049872251456fe9764b
dangchinh25 <dangchinh2501@gmail.com>
2023-09-23 19:07:19 -0500

    Migrate on invocation

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,11 @@
+import { migrate } from 'drizzle-orm/node-postgres/migrator';
 import { livelinessRouter, webhooksRouter } from './collections';
 import { mergeRouters } from './trpc';
+import { dbClient } from '../db';
+
+// This will run the migration everytime there is a coming request
+// In the context of serverless, it is every invocation
+migrate( dbClient, { migrationsFolder: 'drizzle' } );
 
 export const appRouter = mergeRouters(
     livelinessRouter,
