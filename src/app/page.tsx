5f503ef60aa32217e65ca367e27867e0d95a17d7
dangchinh25 <dangchinh2501@gmail.com>
2023-09-25 00:49:27 -0500

    Update code block stype

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,20 +6,19 @@ export default function Home() {
       <p className="text-3xl">Webhook-Er</p>
       <p className="text-xl mt-5 mb-10">It's simple! It's fast! Try it now!</p>
       <CodeBlock
-        code={`
-          curl --location 'webhook-er.vercel.app/api/webhooks'
-          --header 'Content-Type: application/json'
-          --data '{
-              "webhooks": [
-                  {
-                      "payload": {
-                          "data": "TEST"
-                      },
-                      "deliveryAddress": "<YOUR_CUSTOM_URL>"
-                  }
-              ]
-          }'
-        `}
+        code={` curl --location 'webhook-er.vercel.app/api/webhooks'     
+      --header 'Content-Type: application/json'
+      --data '{
+          "webhooks": [
+              {
+                  "payload": {
+                      "data": "TEST"
+                  },
+                  "deliveryAddress": "<YOUR_CUSTOM_URL>"
+              }
+          ]
+      }'
+    `}
         language="bash"
       />
     </main>
