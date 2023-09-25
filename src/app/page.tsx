6ff36fb8495e8db8b11e01351e7c47272124ab4d
dangchinh25 <dangchinh2501@gmail.com>
2023-09-25 00:40:41 -0500

    Add simple landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,27 @@
+import CodeBlock from './_component/CodeBlock';
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center p-8">
+      <p className="text-3xl">Webhook-Er</p>
+      <p className="text-xl mt-5 mb-10">It's simple! It's fast! Try it now!</p>
+      <CodeBlock
+        code={`
+          curl --location 'webhook-er.vercel.app/api/webhooks'
+          --header 'Content-Type: application/json'
+          --data '{
+              "webhooks": [
+                  {
+                      "payload": {
+                          "data": "TEST"
+                      },
+                      "deliveryAddress": "<YOUR_CUSTOM_URL>"
+                  }
+              ]
+          }'
+        `}
+        language="bash"
+      />
+    </main>
   );
 }
