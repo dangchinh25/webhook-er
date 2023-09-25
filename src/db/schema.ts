055e5636fd4db259b6ec114e5b5150c7af0c3160
dangchinh25 <dangchinh2501@gmail.com>
2023-09-25 18:20:18 -0500

    Add type and status

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,15 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import {
-    integer, jsonb, pgTable, serial, text, timestamp
+    integer, jsonb, pgTable, serial, text, timestamp, pgEnum
 } from 'drizzle-orm/pg-core';
+
+export const WebhookType = pgEnum( 'webhook_types', [ 'instant', 'delayed' ] );
+
+export const WebhookStatus = pgEnum( 'webhook_statuses', [
+    'created',
+    'queued',
+    'delivered'
+] );
 
 export const Webhook = pgTable( 'webhooks', {
     id: serial( 'id' ).primaryKey(),
@@ -9,6 +17,9 @@ export const Webhook = pgTable( 'webhooks', {
     deliveryAddress: text( 'delivery_address' ).notNull(),
     attemptNumber: integer( 'attempt_number' ).notNull(),
     deliveredAt: timestamp( 'delivered_at', { withTimezone: true } ),
+    type: WebhookType( 'type' ),
+    status: WebhookStatus( 'status' ).default( 'created' )
+        .notNull(),
     createdAt: timestamp( 'created_at', { withTimezone: true } ).defaultNow(),
     updated: timestamp( 'updated_at', { withTimezone: true } )
 } );
