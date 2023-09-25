0521cb3f039bfe0cd856e515f2e1cd6436422905
dangchinh25 <dangchinh2501@gmail.com>
2023-09-24 19:56:02 -0500

    Add bulk create webhook router

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -6,6 +6,6 @@ export const POST = async (
     req: Request
 ): Promise<Awaited<ReturnType<AppRouter['createWebhook']>>> => {
     return NextResponse.json(
-        await serverClient.createWebhook( await req.json() )
+        await serverClient.createWebhooks( await req.json() )
     );
 };
